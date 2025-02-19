3e56fb2480c3fbd32e13a66e2431786c2f0a09dc
federicofanini <federico.fanini9@gmail.com>
2025-02-19 10:50:52 +0100

    minor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,7 @@
-import { Blog } from "@/components/sections/blog";
-import { Community } from "@/components/sections/community";
 import { CTA } from "@/components/sections/cta";
-import { DemoVideo } from "@/components/sections/demo-video";
-import { Examples } from "@/components/sections/examples";
-import { Features } from "@/components/sections/features";
 import { Footer } from "@/components/sections/footer";
 import { Header } from "@/components/sections/header";
 import { Hero } from "@/components/sections/hero";
-import { Logos } from "@/components/sections/logos";
-import { OldNew } from "@/components/sections/old-new";
-import { Pricing } from "@/components/sections/pricing";
-import { Statistics } from "@/components/sections/statistics";
-import { Testimonials } from "@/components/sections/testimonials";
-import { UseCases } from "@/components/sections/use-cases";
 import { Benefits } from "@/components/sections/benefits";
 import { Profiles } from "@/components/sections/profiles";
 import { getUserCount } from "@/actions/user-count";
@@ -28,10 +17,7 @@ export default async function Home() {
       <Header />
       <Hero count={count} />
       <Profiles />
-      <Pricing />
       <Benefits />
-      {/* <Testimonials />
-      <Statistics /> */}
       <CTA />
       <Footer />
     </main>
